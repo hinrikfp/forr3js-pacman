11731bc4010202427875fb0b1dc46ccfbd458b34
hinrikfp <hinrikfp06@gmail.com>
2025-03-18 12:02:42 +0000

    ghost and point stuff

--- game.js
+++ game.js
@@ -11,16 +11,56 @@ function degToRad(deg) {
 	return deg * Math.PI / 180
 }
 
-let pacman = {
-	location: { x: 100, y: 100 },
-	facing: [0, 1],
-	speed: 100,
-	mouths: {
-		right: [20, 340],
-		left: [200, 160],
-		up: [290, 250],
-		down: [110, 70],
-	},
+class Vec2 {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	addVec(v) {
+		this.x += v.x;
+		this.y += v.y;
+		return this;
+	}
+
+	subVec(v) {
+		this.x -= v.x;
+		this.y -= v.y;
+		return this;
+	}
+
+	mul(n) {
+		this.x *= n;
+		this.y *= n;
+		return this;
+	}
+
+	normalize() {
+		let mag = Math.sqrt(this.x * this.x + this.y * this.y);
+		this.x = this.x / mag;
+		this.y = this.y / mag;
+		return this;
+	}
+
+	normalized() {
+		let mag = Math.sqrt(this.x * this.x + this.y * this.y);
+		return new Vec2(this.x / mag, this.y / mag);
+	}
+}
+
+class Pacman {
+	constructor(location, speed, radius) {
+		this.location = location;
+		this.speed = speed;
+		this.radius = radius;
+		this.mouths = {
+			right: [20, 340],
+			left: [200, 160],
+			up: [290, 250],
+			down: [110, 70],
+		};
+	}
+
 	draw() {
 		ctx.beginPath();
 		// ctx.moveTo(...this.location);
@@ -28,14 +68,15 @@ let pacman = {
 		ctx.arc(
 			this.location.x,
 			this.location.y,
-			20,
+			this.radius,
 			degToRad(this.mouths[inputDirection][0] + (Math.sin(Date.now() * 0.01) * 15.0)),
 			degToRad(this.mouths[inputDirection][1] - (Math.sin(Date.now() * 0.01) * 15.0)),
 		);
 		ctx.lineTo(this.location.x, this.location.y);
 		ctx.fill();
 		ctx.stroke();
-	},
+	}
+
 	update(delta) {
 		switch (inputDirection) {
 			case "right":
@@ -51,23 +92,64 @@ let pacman = {
 				this.location.y += this.speed * delta;
 				break;
 		}
-	},
+	}
 }
 
 class Ghost {
-	constructor(location, color) {
-		this.location = location,
-		this.color = color,
+	constructor(location, color, radius) {
+		this.location = location;
+		this.color = color;
+		this.radius = radius;
 	}
 
 	draw() {
 		ctx.beginPath();
-		ctx.fillStyle = color;
+		// ctx.moveTo(...this.location);
+		ctx.fillStyle = this.color;
+		ctx.arc(
+			this.location.x,
+			this.location.y,
+			this.radius,
+			degToRad(180),
+			degToRad(360),
+		);
+		ctx.lineTo(this.location.x + this.radius, this.location.y + this.radius);
+		ctx.lineTo(this.location.x - this.radius, this.location.y + this.radius);
+		ctx.fill();
+		ctx.stroke();
+	}
+
+	update(delta) {
+		this.location.x += 10 * delta;
 	}
 }
 
+class Point {
+	constructor(location, worth, color, radius) {
+		this.location = location;
+		this.worth = worth;
+		this.radius = radius;
+		this.color = color;
+	}
+
+	draw() {
+		ctx.beginPath();
+		ctx.fillStyle = this.color;
+		ctx.arc(this.location.x, this.location.y, this.radius, 0, 360);
+		ctx.fill();
+		ctx.stroke();
+	}
+
+	update(delta) { }
+}
+
+let pacman = new Pacman(new Vec2(100, 100), 100, 15);
+
 let gameObjects = [
 	pacman,
+	new Ghost(new Vec2(200, 200), "red", 15),
+	new Ghost(new Vec2(250, 200), "blue", 15),
+	new Point(new Vec2(100, 100), 10, "#ebcb4b", 5),
 ]
 
 function drawSquare(x, y, size, color) {
@@ -77,7 +159,7 @@ function drawSquare(x, y, size, color) {
 
 function drawBg(color) {
 	ctx.fillStyle = color;
-	ctx.fillRect(0,0,canvas.width, canvas.height);
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function gameInput(event) {
